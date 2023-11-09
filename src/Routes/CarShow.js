625e517fff75c00a7826cf554268ce63e930c982
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-11-09 13:43:50 +0300

    created cardata

--- src/Routes/CarShow.js
+++ src/Routes/CarShow.js
@@ -3,15 +3,18 @@ import NavBar from '../components/NavBar'
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { Container } from 'react-bootstrap';
+import { CarData } from '../components/CarData';
 function CarShow() {
   return (
     <div>
       <NavBar />
-      <Row xs={1} md={2} lg={4} className="g-4 mt-2">
+      <Container fluid>
+      {/* <Row xs={1} md={2} lg={4} className="g-4 mt-2">
       
         <Col >
-          <Card className='mx-1' >
-            <Card.Img variant="top" src="https://picsum.photos/1000?random=1" height={"300px"}/>
+          <Card className='' >
+            <Card.Img variant="top" src={ranger} height={"300px"}/>
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -24,7 +27,7 @@ function CarShow() {
           
         </Col>
         <Col>
-          <Card className='mx-1' >
+          <Card className='' >
             <Card.Img variant="top" src="https://picsum.photos/1000?random=2" height={"300px"}/>
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -38,7 +41,7 @@ function CarShow() {
           
         </Col>
         <Col>
-          <Card className='mx-1' >
+          <Card className='' >
             <Card.Img variant="top" src="https://picsum.photos/1000?random=3" height={"300px"}/>
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -52,7 +55,7 @@ function CarShow() {
           
         </Col>
         <Col>
-          <Card className='mx-1' >
+          <Card className='' >
             <Card.Img variant="top" src="https://picsum.photos/1000?random=4" height={"300px"}/>
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -65,8 +68,12 @@ function CarShow() {
           </Card>
           
         </Col>
-        <Col>
-          <Card className='mx-1' >
+       
+      
+    </Row>
+    <Row xs={1} md={2} lg={4} className="g-4 mt-2">
+    <Col>
+          <Card className='' >
             <Card.Img variant="top" src="https://picsum.photos/1000?random=5" height={"300px"}/>
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -79,8 +86,65 @@ function CarShow() {
           </Card>
           
         </Col>
-      
+        <Col>
+          <Card className='' >
+            <Card.Img variant="top" src="https://picsum.photos/1000?random=5" height={"300px"}/>
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          
+        </Col>
+        <Col>
+          <Card className='' >
+            <Card.Img variant="top" src="https://picsum.photos/1000?random=5" height={"300px"}/>
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          
+        </Col>
+        <Col>
+          <Card className='' >
+            <Card.Img variant="top" src="https://picsum.photos/1000?random=5" height={"300px"}/>
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          
+        </Col>
+        </Row> */}
+         <Row xs={1} md={2} lg={4} className="g-4 mt-2">
+      {Array.from(CarData).map((data, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Img variant="top" src={data.img} height={250} />
+            <Card.Body> 
+              <Card.Title>{data.name}</Card.Title>
+              <Card.Text className='text-center bg-success text-white'>
+                {data.price} €
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
     </Row>
+        </Container>
     </div>
   )
 }
---
+++ src/components/CarData.js
@@ -0,0 +1,113 @@
+import ranger from "../assets/ranger.jpeg"
+import mcls from "../assets/2005_Mercedes-Benz_CLS_320_CDi_Automatic_3.0_Rear.jpg"
+import audiq7 from "../assets/audiq7.jpg"
+import audia7 from "../assets/2015-audi-a7-30-tfsi-quattro-progressiv_1.png"
+import camry from "../assets/toyota camry.jpeg"
+import minic from "../assets/minic.jpeg"
+import bmw from "../assets/2023bmw.jpeg"
+import mits from "../assets/Mitsubishi_ASX_front_20100717.jpg"
+import peugeot from "../assets/peugeot.jpg"
+import chev from "../assets/chevrolet.jpeg"
+
+export const CarData = [
+    {
+        id:1,
+        name:"2016 LAND ROVER RANGE ROVER AUTOBIOGRAPHI 3.0 DİZEL HYIBRID",
+        img:ranger,
+        price:"55000",
+        year:"2016",
+        body:"SUV",
+        Cylinder:"6 Cylinder",
+        Color:"Black"
+    },
+    {
+        id:2,
+        name:"2005 MERCEDES CLS 320 CDI",
+        img:mcls,
+        price:"9000",
+        year:"2015",
+        body:"Sedan",
+        Cylinder:"4 Cylinder",
+        Color:"Black"
+    },
+    {
+        id:3,
+        name:"2014 AUDI Q7 3.0 T QUATTRO 7 SEATER",
+        img:audiq7,
+        price:"19000",
+        year:"2014",
+        body:"SUV",
+        Cylinder:"6 Cylinder",
+        Color:"White"
+    },
+    {
+        id:4,
+        name:"2015 AUDI A7 3.0 TFSI QUATTRO S-LİNE",
+        img:audia7,
+        price:"31000",
+        year:"2015",
+        body:"Coupe",
+        Cylinder:"6 Cylinder",
+        Color:"Grey"
+    },
+    {
+        id:5,
+        name:"2023 TOYOTA CAMMRY HYBRITH 2.5",
+        img:camry,
+        price:"40000",
+        year:"2023",
+        body:"Sedan",
+        Cylinder:"4 Cylinder",
+        Color:"Black"
+    },
+    {
+        id:6,
+        name:"2022 MINI COOPER 1.5",
+        img:minic,
+        price:"30000",
+        year:"2022",
+        body:"5 Door Hatchback",
+        Cylinder:"4 Cylinder",
+        Color:"Black"
+    },
+    {
+        id:7,
+        name:"2023 BMW X1 sDrive 18i M Sport",
+        img:bmw,
+        price:"45000",
+        year:"2023",
+        body:"SUV",
+        Cylinder:"4 Cylinder",
+        Color:"White"
+    },
+    {
+        id:8,
+        name:"2012 MITSUBISHI ASX1.6",
+        img:mits,
+        price:"11000",
+        year:"2012",
+        body:"SUV",
+        Cylinder:"4 Cylinder",
+        Color:"Black"
+    },
+    {
+        id:9,
+        name:"2010 PEUGEOT 3008 1.6",
+        img:peugeot,
+        price:"9000",
+        year:"2010",
+        body:"SUV",
+        Cylinder:"4 Cylinder",
+        Color:"Grey"
+    },
+    {
+        id:10,
+        name:"2014 CHEVROLET CRUİZE 1.4",
+        img:chev,
+        price:"10000",
+        year:"2014",
+        body:"Sedan",
+        Cylinder:"4 Cylinder",
+        Color:"Red"
+    }
+]
